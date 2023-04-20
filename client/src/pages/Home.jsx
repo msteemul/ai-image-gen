356e33111351a1f42c233a5b2302777ba40f88a0
msteemul <113412804+msteemul@users.noreply.github.com>
2023-04-20 16:37:20 +0400

    home component completed

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -5,6 +5,16 @@ const Home = () => {
    const [loading, setLoading] = useState(false)
     const [allPost, setallPost] = useState(null)
     const [searchText, setSearchText] = useState('')
+    const RenderCards = ({data,title}) => {
+      if(data?.length >0) {
+        return data.map((post) => <Card key={post._id} {...post} />)
+      }
+
+      return(
+        <h2 className='mt-5 font-bold text-[#6449ff] text-xl uppercase'>{title}</h2>
+      )
+
+    }
   return (
    <section className='max-w-7xl mx-auto'>
     <div>
@@ -24,8 +34,24 @@ const Home = () => {
       ):(
         <>
         {searchText && (
-          <h2></h2>
+          <h2 className='font-medium text-[#666e75] text-xl mb-3'>
+            Showing results for
+            <span className='text-[#222328]'>{searchText}</span>
+          </h2>
         )}
+        <div className='grid lg:grid-cols-4 sm:grid-col-3 xs:grid-cols-2 grid-cols-1 gap-3'>
+          {searchText ? (
+            <RenderCards 
+            data={[]}
+            title="No search results found"
+            />
+          ): (
+            <RenderCards 
+            data={[]}
+            title="No posts found"
+            />
+          )}
+        </div>
         
         </>
       )}
