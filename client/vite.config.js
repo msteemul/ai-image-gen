ee239bfb418e76d539feb684de97e67aaccf7052
msteemul <113412804+msteemul@users.noreply.github.com>
2023-04-21 00:26:29 +0400

    config

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000 // Replace 3000 with the desired port number
+  }
 })
